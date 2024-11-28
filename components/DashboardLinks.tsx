8f84f020432dd213125eb1574f91fcce82bdc307
Antonis Papaioannou <mrantonp@gmail.com>
2024-11-28 19:20:58 +0200

    add dashboard layout and navigation components; include logo and dropdown menu functionality

---
+++ components/DashboardLinks.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { cn } from "@/lib/utils"
+import { HomeIcon, Users2 } from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+
+export const dashboardLinks = [
+  {
+    id: 0,
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: HomeIcon
+  },
+  {
+    id: 1,
+    name: "Invoices",
+    href: "/dashboard/invoices",
+    icon: Users2
+  },
+]
+
+
+export function DashboardLinks() {
+  const pathname = usePathname()
+  return (
+    <>
+      {dashboardLinks.map((link) => (
+        <Link href={link.href} key={link.id} className={cn(
+          pathname === link.href ? "bg-primary/10 text-primary" : "text-muted-foreground hover:text-foreground", "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
+        )}>
+          <link.icon className="size-4" />
+          {link.name}
+        </Link>
+      ))}
+    </>
+  )
+}
